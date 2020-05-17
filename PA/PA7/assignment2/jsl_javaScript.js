7c59fc7fc6f3be082df19d7eaa346e799fe46d4b
JaesungLeee <viki0114@naver.com>
2020-05-17 20:08:57 +0900

    update PA6, PA7

---
+++ PA/PA7/assignment2/jsl_javaScript.js
@@ -0,0 +1,11 @@
+function myImage() {
+    var m;
+    m = document.getElementById("img_Id");
+    m.src = "jslimg.png";
+}
+
+function noImage() {
+    var m;
+    m = document.getElementById("img_Id");
+    m.src = "";
+}
